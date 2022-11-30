56c33e05c9a362f3dcd33d8b90b9d55b76cfcdb9
bclabs-theo <theo.kim@bclabs.co.kr>
2022-11-30 16:57:17 +0900

    add: bundle files

---
+++ dist/bundle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var Test = function Test(_ref) {
+    var children = _ref.children;
+    return /*#__PURE__*/ React.createElement("div", {
+        className: "bg-green-800"
+    }, children);
+};
+
+export { Test };
+//# sourceMappingURL=bundle.js.map
