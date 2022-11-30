7aa611dee81793a34d76ae8bf8f9cd8a4a5a058d
bclabs-theo <theo.kim@bclabs.co.kr>
2022-11-30 16:28:15 +0900

    refac: 경로 설정 및 코드 정리

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,24 @@
 import { babel } from '@rollup/plugin-babel';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
 
 const extensions = [ 'js', 'jsx', 'ts', 'tsx', 'mjs' ];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const paths = {
+    input: resolve(__dirname, "./src/index.ts"),
+    file: resolve(__dirname, "./dist/bundle.js"),
+};
+
 const config =  {
-    input: "./src/index.ts",
+    input: paths.input,
     output: {
-        file: "./dist/bundle.js",
+        file: paths.file,
         format: "es",
         sourcemap: true,
     },
