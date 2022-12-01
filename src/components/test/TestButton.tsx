4fdc3cce2688e6b53c990ea2ee54c2d969f9f58f
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-01 13:20:31 +0900

    fix: 테스트 스타일 인라인 적용'

--- src/components/test/TestButton.tsx
+++ src/components/test/TestButton.tsx
@@ -5,7 +5,7 @@ type TestProps = {
     children: React.ReactNode
 }
 export const TestButton: React.FC<TestProps> = ({children}) => {
-    return <button className="bg-green-800 hover:bg-cyan-600 test-style">
+    return <button style={{border: "2px dotted red;"}} className="bg-green-800 hover:bg-cyan-600">
         {children}
     </button>
 }
