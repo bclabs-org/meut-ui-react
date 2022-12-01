b987692992dd89a361efd50944b2dad264341319
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-01 13:17:28 +0900

    fix: 테스트 스타일 적용

--- src/components/test/TestButton.tsx
+++ src/components/test/TestButton.tsx
@@ -5,7 +5,7 @@ type TestProps = {
     children: React.ReactNode
 }
 export const TestButton: React.FC<TestProps> = ({children}) => {
-    return <button className="bg-green-800">
+    return <button className="bg-green-800 hover:bg-cyan-600 test-style">
         {children}
     </button>
 }
