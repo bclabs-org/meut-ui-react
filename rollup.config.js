eb5446826bdc260ac6bac9e46e5a6f2f929cdd3b
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-01 14:55:42 +0900

    ref: 코드 정리

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import typescript from "rollup-plugin-typescript2";
-import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 
 const extensions = [ 'js', 'jsx', 'ts', 'tsx', 'mjs' ];
 
@@ -16,7 +16,7 @@ const paths = {
     file: resolve(__dirname, "./dist/bundle.js"),
 };
 
-const config =  {
+export default  {
     input: paths.input,
     output: {
         file: paths.file,
@@ -24,26 +24,22 @@ const config =  {
         sourcemap: true,
     },
     plugins: [
-        nodeResolve({extensions}),
-        babel({
-            babelHelpers: "runtime",
-            exclude: "node_modules/**",
-            extensions
-        }),
-        commonjs({include: /node_modules/}),
-        typescript({ useTsconfigDeclarationDir: true }),
         postcss({
-            config:{
-                path: resolve(__dirname, "./postcss.config.js"),
-            },
+            extensions: ['.css'],
             minimize: true,
             inject: 'top',
             extract: true,
             sourceMap: false,
             use: ['sass'],
         }),
+        babel({
+            babelHelpers: "runtime",
+            exclude: "node_modules/**",
+            extensions,
+        }),
+        terser(),
+        typescript({ useTsconfigDeclarationDir: true }),
+        nodeResolve({extensions}),
     ],
     external: ["react", "react-dom", "typescript", '@babel/runtime']
 };
-
-export default config;
