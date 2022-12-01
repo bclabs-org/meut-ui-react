396cea5a394126bcfcca7bc3a249057906dd3bbb
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-01 13:17:45 +0900

    fix: bundle files'

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,7 +3,7 @@ import React from 'react';
 var TestButton = function TestButton(_ref) {
     var children = _ref.children;
     return /*#__PURE__*/ React.createElement("button", {
-        className: "bg-green-800"
+        className: "bg-green-800 hover:bg-cyan-600"
     }, children);
 };
 
