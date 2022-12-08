790efdeee1fdb8d62176fad5c4ad65357dc540fb
miakim <mia.kim@bclabs.co.kr>
2022-12-08 09:55:16 +0900

    feat: 공통으로 사용하는 컬러 추가

--- src/components/test/TestButton.tsx
+++ src/components/test/TestButton.tsx
@@ -4,7 +4,7 @@ type TestProps = {
     children: React.ReactNode
 }
 export const TestButton: React.FC<TestProps> = ({children}) => {
-    return <button className="bg-green-800 hover:bg-cyan-600 text-white font-bold py-2 px-4 rounded">
+    return <button className="bg-primary hover:bg-primary-hover text-white font-bold py-2 px-4 rounded">
         {children}
     </button>
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,47 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6CD28A',
+          disabled: 'rgba(108, 210, 138, 0.4)',
+        },
+        secondary: {
+          DEFAULT: '#8B63E4',
+          disabled: 'rgba(139, 99, 228, 0.4)',
+        },
+        tertiary: {
+          DEFAULT: '#4E73CF',
+          disabled: 'rgba(78, 115, 207, 0.4)',
+        },
+      }
+    },
   },
-  plugins: [],
-  corePlugins: { preflight: false },
+  plugins: [
+    plugin(function ({addComponents, theme}) {
+      addComponents(({
+        '.bg-primary-hover': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), ${theme('colors.primary.DEFAULT')}`
+        },
+        '.bg-primary-active': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), ${theme('colors.primary.DEFAULT')}`
+        },
+        '.bg-secondary-hover': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), ${theme('colors.secondary.DEFAULT')}`
+        },
+        '.bg-secondary-active': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), ${theme('colors.secondary.DEFAULT')}`
+        },
+        '.bg-tertiary-hover': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.03), rgba(0, 0, 0, 0.03)), ${theme('colors.tertiary.DEFAULT')}`
+        },
+        '.bg-tertiary-active': {
+          background: `linear-gradient(0deg, rgba(0, 0, 0, 0.07), rgba(0, 0, 0, 0.07)), ${theme('colors.tertiary.DEFAULT')}`
+        }
+      }))
+    })
+  ],
 }
