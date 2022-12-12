c861b2cd5b5a82e9f8e5591e917c35c62c6cf588
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-12 11:18:49 +0900

    add: ProfilePhoto component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,8 @@ import './index.css';
 import TestButton from './Test';
 import Card from './Card';
 import Button from './Button';
+import ProfilePhoto from './ProfilePhoto';
 
-export { TestButton, Button, Card };
+export {
+  TestButton, Button, Card, ProfilePhoto,
+};
