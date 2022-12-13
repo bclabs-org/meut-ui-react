20ddd2c157afdf336e85d805381add6d606506fd
miakim <mia.kim@bclabs.co.kr>
2022-12-13 09:28:49 +0900

    feat: storybook 적용

--- src/components/test/TestButton.tsx
+++ src/components/test/TestButton.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 type TestProps = {
     children: React.ReactNode
 }
-
-const TestButton: React.FC<TestProps> = ({ children }) => <button className="bg-primary hover:bg-primary-hover text-white font-bold py-2 px-4 rounded-100 shadow-light-100">
+export const TestButton: React.FC<TestProps> = ({ children }) => <button className="bg-primary hover:bg-primary-hover text-white font-bold py-2 px-4 rounded-100 shadow-light-100">
         {children}
     </button>;
-
-export default TestButton;
