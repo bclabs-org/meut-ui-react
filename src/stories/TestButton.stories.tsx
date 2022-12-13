88078aea6f52261811888727bb2890d0e58db13c
miakim <mia.kim@bclabs.co.kr>
2022-12-13 11:12:02 +0900

    feat: 스토리북에 테스트 컴포넌트 추가

---
+++ src/stories/TestButton.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TestButton} from "../components/test/TestButton";
+
+export default {
+  component: TestButton,
+  title: 'Test/TestButton',
+}
+
+const Template = (args) => <TestButton {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'assadasd'
+}
