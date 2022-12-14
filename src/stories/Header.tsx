4ed227574b66148e36490f0d3cbc733018fa9e80
miakim <mia.kim@bclabs.co.kr>
2022-12-14 11:37:56 +0900

    style: lint fix

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -14,7 +14,9 @@ interface HeaderProps {
   onCreateAccount: () => void;
 }
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+export const Header = ({
+  user, onLogin, onLogout, onCreateAccount,
+}: HeaderProps) => (
   <header>
     <div className="wrapper">
       <div>
