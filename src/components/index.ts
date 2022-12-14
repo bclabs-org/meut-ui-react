7e76cc909ef5449b1a33745a233ff05aad690d5d
kate <kate.choi@bclabs.co.kr>
2022-12-14 15:35:19 +0900

    delete: test button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import './index.css';
-import TestButton from './Test';
 import Card from './Card';
 import Button from './Button';
 import Modal from './Modal'
 
-export { TestButton, Button, Card, Modal };
+export { Button, Card, Modal };
