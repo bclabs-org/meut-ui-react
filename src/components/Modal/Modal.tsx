f9713eeca7367c4168fe6a9b3d95219f43fdb101
literate <gaekoon@gmail.com>
2022-12-15 22:26:04 +0900

    fix: 가독성을 위해 setIsModalOpen -> () => setIsModalOpen(false)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,44 +5,50 @@ type ModalProps = {
   children: React.ReactNode;
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}
+};
 
-const Modal: React.FC<ModalProps> = ({ children, isModalOpen, setIsModalOpen }) => (
-    <Transition.Root show={isModalOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => setIsModalOpen(false)}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+const Modal: React.FC<ModalProps> = ({
+  children,
+  isModalOpen,
+  setIsModalOpen,
+}) => (
+  <Transition.Root show={isModalOpen} as={Fragment}>
+    <Dialog
+      as="div"
+      className="relative z-10"
+      onClose={() => setIsModalOpen(false)}
+    >
+      <Transition.Child
+        as={Fragment}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="w-[496px] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
-                <div className="p-8">
-                  {children}
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <Dialog.Panel className="w-[496px] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+              <div className="p-8">{children}</div>
+            </Dialog.Panel>
+          </Transition.Child>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </div>
+    </Dialog>
+  </Transition.Root>
 );
 
 export default Modal;
