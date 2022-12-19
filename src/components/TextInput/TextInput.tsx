f122d3861d18422003fda261d4df6a7a5363bc0b
kate <kate.choi@bclabs.co.kr>
2022-12-19 16:49:54 +0900

    add: autocomplete off

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -42,6 +42,7 @@ const TextInput: React.FC<TextInputProps> = ({
         placeholder={placeholder}
         onChange={onChange}
         value={value}
+        autoComplete="off"
       />
     </div>
     {errorMessage ? (
