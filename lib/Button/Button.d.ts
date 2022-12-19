0a1aa0dddc219c5b6e83db9f5e1ea8789072defa
bclabs-theo <theo.kim@bclabs.co.kr>
2022-12-19 18:08:08 +0900

    fix: bundle files

--- lib/Button/Button.d.ts
+++ lib/Button/Button.d.ts
@@ -3,6 +3,8 @@ type ButtonProps = {
     children: React.ReactNode;
     color: string;
     size: string;
+    disabled?: boolean;
+    [key: string]: any;
 };
 declare const Button: React.FC<ButtonProps>;
 export default Button;
