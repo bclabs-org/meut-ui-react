eef004768908f638bb0c3b513bdece7e6c301d56
kate <kate.choi@bclabs.co.kr>
2022-12-19 16:54:33 +0900

    delete: error icon pb 삭제

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -47,7 +47,7 @@ const TextInput: React.FC<TextInputProps> = ({
     </div>
     {errorMessage ? (
       <div className="flex items-center">
-        <div className="pb-0.5">
+        <div>
           <svg width="14" height="14" viewBox="0 0 14 14" fill="none"
                xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd"
