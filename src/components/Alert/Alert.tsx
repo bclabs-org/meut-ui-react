bac8959265eb73fc029a3aab96cc06917a922313
kate <kate.choi@bclabs.co.kr>
2023-01-03 16:02:38 +0900

    prettier

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,9 +1,5 @@
-import React, { MouseEventHandler } from "react";
-import {
-  XCircleIcon,
-  XMarkIcon,
-  ExclamationTriangleIcon,
-} from "@heroicons/react/20/solid";
+import React, { MouseEventHandler } from 'react';
+import { XCircleIcon, XMarkIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid';
 
 type AlertProps = {
   text: string;
@@ -15,54 +11,47 @@ type AlertProps = {
 
 const Alert: React.FC<AlertProps> = ({
   text,
-  color = "error",
+  color = 'error',
   setIsAlertOpen,
   closeBtn = false,
-  animation = "",
+  animation = '',
 }: AlertProps) => {
   let alertBackground;
   let alertIcon;
   let alertText;
   switch (color) {
-    case "error":
-      alertBackground = "bg-pink-50";
-      alertIcon = "text-pink-600";
-      alertText = "text-pink-800";
+    case 'error':
+      alertBackground = 'bg-pink-50';
+      alertIcon = 'text-pink-600';
+      alertText = 'text-pink-800';
       break;
-    case "attention":
-      alertBackground = "bg-yellow-50";
-      alertIcon = "text-yellow-400";
-      alertText = "text-yellow-800";
+    case 'attention':
+      alertBackground = 'bg-yellow-50';
+      alertIcon = 'text-yellow-400';
+      alertText = 'text-yellow-800';
       break;
     default:
-      throw Error("invalid color value");
+      throw Error('invalid color value');
   }
   console.log(color, animation);
 
   let alertAnimation;
   switch (animation) {
-    case "fromTop":
-      alertAnimation = "top-[96px] show";
+    case 'fromTop':
+      alertAnimation = 'top-[96px] show';
       break;
-    case "":
-      alertAnimation = "";
+    case '':
+      alertAnimation = '';
       break;
     default:
-      throw Error("invalid animation value");
+      throw Error('invalid animation value');
   }
 
   const renderIcon = () => {
-    if (color === "error") {
-      return (
-        <XCircleIcon className={`${alertIcon} w-5 h-5`} aria-hidden="true" />
-      );
+    if (color === 'error') {
+      return <XCircleIcon className={`${alertIcon} w-5 h-5`} aria-hidden="true" />;
     }
-    return (
-      <ExclamationTriangleIcon
-        className={`${alertIcon} w-5 h-5`}
-        aria-hidden="true"
-      />
-    );
+    return <ExclamationTriangleIcon className={`${alertIcon} w-5 h-5`} aria-hidden="true" />;
   };
 
   return (
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 
 type ButtonProps = {
   children: React.ReactNode;
@@ -12,7 +12,7 @@ type ButtonProps = {
 
 const Button: React.FC<ButtonProps> = ({
   children,
-  color = "primary",
+  color = 'primary',
   size,
   disabled,
   handleClick,
@@ -20,37 +20,35 @@ const Button: React.FC<ButtonProps> = ({
 }: ButtonProps) => {
   let btnSize;
   switch (size) {
-    case "x-large":
-      btnSize = "w-full h-[64px] text-[20px]";
+    case 'x-large':
+      btnSize = 'w-full h-[64px] text-[20px]';
       break;
-    case "large":
-      btnSize = "h-[64px] text-[20px] py-[20px] px-[40px]";
+    case 'large':
+      btnSize = 'h-[64px] text-[20px] py-[20px] px-[40px]';
       break;
-    case "medium":
-      btnSize = "h-[48px] text-[16px] py-[14px] px-[24px]";
+    case 'medium':
+      btnSize = 'h-[48px] text-[16px] py-[14px] px-[24px]';
       break;
-    case "small":
-      btnSize = "h-[32px] text-[14px] py-[8px] px-[16px]";
+    case 'small':
+      btnSize = 'h-[32px] text-[14px] py-[8px] px-[16px]';
       break;
     default:
-      throw Error("invalid size value");
+      throw Error('invalid size value');
   }
 
   let btnColor;
   switch (color) {
-    case "primary":
-      btnColor = "bg-primary hover:bg-primary-hover active:bg-primary-active";
+    case 'primary':
+      btnColor = 'bg-primary hover:bg-primary-hover active:bg-primary-active';
       break;
-    case "secondary":
-      btnColor =
-        "bg-secondary hover:bg-secondary-hover active:bg-secondary-active";
+    case 'secondary':
+      btnColor = 'bg-secondary hover:bg-secondary-hover active:bg-secondary-active';
       break;
-    case "tertiary":
-      btnColor =
-        "bg-tertiary hover:bg-tertiary-hover active:bg-tertiary-active";
+    case 'tertiary':
+      btnColor = 'bg-tertiary hover:bg-tertiary-hover active:bg-tertiary-active';
       break;
     default:
-      throw Error("invalid color value");
+      throw Error('invalid color value');
   }
 
   const styleClass = classNames(btnSize, btnColor);
