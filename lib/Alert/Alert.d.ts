707996df67bcf82e76e5b5066b4359dc1cdcb702
kimpaul <paul.kim@bclabs.co.kr>
2023-01-03 10:21:20 +0900

    feat/PLAYV-1070 #comment PLAYV-1070 "Alert에 Attention 추가"

--- lib/Alert/Alert.d.ts
+++ lib/Alert/Alert.d.ts
@@ -1,7 +1,10 @@
 import React from 'react';
+
 type AlertProps = {
     text: string;
     color?: string;
+    shadow?: boolean;
+    closeBtn?: boolean;
 };
 declare const Alert: React.FC<AlertProps>;
 export default Alert;
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { XCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
+import { XCircleIcon, XMarkIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid';
 
 type AlertProps = {
   text: string;
   color?: string;
+  shadow?: boolean;
+  closeBtn?: boolean;
 };
 
-const Alert: React.FC<AlertProps> = ({ text, color = 'error' }: AlertProps) => {
+const Alert: React.FC<AlertProps> = ({
+  text, color = 'error', shadow = false, closeBtn = false,
+}: AlertProps) => {
   let alertBackground;
   let alertIcon;
   let alertText;
@@ -17,30 +21,41 @@ const Alert: React.FC<AlertProps> = ({ text, color = 'error' }: AlertProps) => {
       alertIcon = 'text-pink-600';
       alertText = 'text-pink-800';
       break;
+    case 'attention':
+      alertBackground = 'bg-yellow-50';
+      alertIcon = 'text-yellow-400';
+      alertText = 'text-yellow-800';
+      break;
     default:
       throw Error('invalid color value');
   }
 
+  const renderIcon = () => {
+    if (color === 'error') {
+      return <XCircleIcon className={`${alertIcon} w-5 h-5`} aria-hidden="true" />;
+    }
+    return <ExclamationTriangleIcon className={`${alertIcon} w-5 h-5`} aria-hidden="true" />;
+  };
   return (
-    <div className={`${alertBackground} rounded-md p-4 w-max`}>
+    <div className={`${alertBackground} rounded-md p-4 w-max ${shadow && 'shadow-lg'}`}>
       <div className="flex h-[20px]">
         <div>
-          <XCircleIcon className={`${alertIcon} h-5 w-5`} aria-hidden="true" />
+          {renderIcon()}
         </div>
         <div className="ml-3">
           <p className={`${alertText} text-sm font-medium`}>{text}</p>
         </div>
         <div className="ml-3">
-            <button
-              type="button"
-              className={`inline-flex items-center ${alertBackground} ${alertIcon}`}
-            >
-              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-            </button>
+          <button
+            type="button"
+            className={`inline-flex items-center ${alertBackground} ${alertIcon}`}
+          >
+            {closeBtn && <XMarkIcon className="h-5 w-5" aria-hidden="true" /> }
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Alert;
