9bf8ffdfd8cb494dca2ce858aae1868072e2f6b8
bclabs-theo <theo.kim@bclabs.co.kr>
2023-01-04 10:08:10 +0900

    fix: lint fix 및 코드 수정으로 인한 bundle file rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -2,49 +2,49 @@ import { babel } from '@rollup/plugin-babel';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from "@rollup/plugin-commonjs";
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import typescript from "rollup-plugin-typescript2";
-import terser from "@rollup/plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
 
-const extensions = [ 'js', 'jsx', 'ts', 'tsx', 'mjs' ];
+const extensions = ['js', 'jsx', 'ts', 'tsx', 'mjs'];
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
 
 const paths = {
-    input: resolve(__dirname, "./src/components/index.ts"),
-    file: resolve(__dirname, "./dist/bundle.js"),
+  input: resolve(dirName, './src/components/index.ts'),
+  file: resolve(dirName, './dist/bundle.js'),
 };
 
-export default  {
-    input: paths.input,
-    output: {
-        file: paths.file,
-        format: "esm",
-        sourcemap: true,
-    },
-    plugins: [
-        typescript({ useTsconfigDeclarationDir: true }),
-        postcss({
-            config: {
-                path: "./postcss.config.cjs",
-            },
-            extensions: [".css"],
-            minimize: true,
-            use: ["sass"],
-            inject: {
-                insertAt: "top",
-            },
-        }),
-        babel({
-            babelHelpers: "bundled",
-            exclude: "node_modules/**",
-            extensions,
-        }),
-        commonjs({ include: "node_modules/**" }),
-        nodeResolve({extensions}),
-        terser(),
-    ],
-    external: ["react", "react-dom", "typescript"]
+export default {
+  input: paths.input,
+  output: {
+    file: paths.file,
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      config: {
+        path: './postcss.config.cjs',
+      },
+      extensions: ['.css'],
+      minimize: true,
+      use: ['sass'],
+      inject: {
+        insertAt: 'top',
+      },
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+    commonjs({ include: 'node_modules/**' }),
+    nodeResolve({ extensions }),
+    terser(),
+  ],
+  external: ['react', 'react-dom', 'typescript'],
 };
