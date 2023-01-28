d7d5d04ce5f1d8a85a7279913ce449b10b276f6e
kate <kate.choi@bclabs.co.kr>
2023-01-29 02:56:54 +0900

    add: dropdown component

--- lib/Modal/Modal.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-type ModalProps = {
-    children: React.ReactNode;
-    isModalOpen: boolean;
-    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
-declare const Modal: React.FC<ModalProps>;
-export default Modal;
--- lib/ProfilePhoto/ProfilePhoto.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './style.css';
-type ProfilePhotoProps = {
-    size: 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | '2x-large';
-    imageFile: string;
-};
-declare const ProfilePhoto: React.FC<ProfilePhotoProps>;
-export default ProfilePhoto;
