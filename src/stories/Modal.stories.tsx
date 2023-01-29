6dd9aba3cd5eeea7c177f2250f34342fe5d9b94a
kate <kate.choi@bclabs.co.kr>
2023-01-29 17:53:45 +0900

    remove React code

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Modal from '../components/Modal/Modal';
--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Tabs from '../components/Tabs/Tabs';
