d2a54c0b6e369346b4bba9f70fd8852224d27262
kimpaul <paul.kim@bclabs.co.kr>
2023-01-31 21:45:36 +0900

    fix/PLAYV-1186 #comment PLAYV-1217 "save 버튼이 있을경우 렌더링조건 추가"

--- src/components/TextareaInput/TextareaInput.tsx
+++ src/components/TextareaInput/TextareaInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Button';
 
 type TextareaInputProps = {
   label: string;
@@ -9,6 +10,8 @@ type TextareaInputProps = {
   placeholder?: string;
   disabled?: boolean;
   onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  saveButton?: boolean;
+  handleSaveButtonClick?: () => void;
   error?: boolean;
   [key: string]: any;
 };
@@ -23,11 +26,13 @@ const TextareaInput = ({
   error,
   maxByteSize,
   byteSize,
+  saveButton,
+  handleSaveButtonClick,
   ...rest
 }: TextareaInputProps) => {
   const getTextareaInputClass = () => {
     const defaultClass =
-      'block mt-1 w-[468px] h-[105px] border border-gray-300 rounded-md text-base text-gray-900 font-medium';
+      'block mt-1 w-full h-[105px] border border-gray-300 rounded-md text-base text-gray-900 font-medium';
 
     if (disabled) {
       return `${defaultClass}`;
@@ -40,8 +45,30 @@ const TextareaInput = ({
     return `${defaultClass} hover:ring-[3px] hover:ring-emerald-100 focus:ring-1 focus:ring-emerald-500 focus:border-emerald-500`;
   };
 
+  const renderTextareaFooter = () => {
+    if (maxByteSize && saveButton) {
+      return (
+        <div className={`flex justify-between items-center mt-2`}>
+          <p className="text-sm">
+            {`${value?.length} Word(s) / ${byteSize} Byte(s) (Total ${maxByteSize} Bytes)`}
+          </p>
+          <Button size={'small'} color={'tertiary'} handleClick={handleSaveButtonClick}>
+            Save
+          </Button>
+        </div>
+      );
+    }
+    if (maxByteSize) {
+      return (
+        <p className="mt-1 text-sm m-0 p-0">
+          {`${value?.length} Word(s) / ${byteSize} Byte(s) (Total ${maxByteSize} Bytes)`}
+        </p>
+      );
+    }
+  };
+
   return (
-    <div className={`${disabled ? `opacity-40 grayscale` : ``}`}>
+    <div className={`w-[468px] ${disabled ? `opacity-40 grayscale` : ``}`}>
       <label htmlFor={name} className="block text-sm font-medium">
         {label}
       </label>
@@ -54,11 +81,7 @@ const TextareaInput = ({
         disabled={disabled}
         {...rest}
       />
-      {maxByteSize ? (
-        <p className="mt-1 text-sm m-0 p-0">
-          {`${value?.length} Word(s) / ${byteSize} Byte(s) (Total ${maxByteSize} Bytes)`}
-        </p>
-      ) : null}
+      {renderTextareaFooter()}
     </div>
   );
 };
