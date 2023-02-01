5e6129a31071a36033accd2c4357954f8090cdf9
kimpaul <paul.kim@bclabs.co.kr>
2023-02-01 14:34:55 +0900

    fix/PLAYV-1186 #comment PLAYV-1186 "스토리북 케이스 추가"

--- src/stories/TextareaInput.stories.tsx
+++ src/stories/TextareaInput.stories.tsx
@@ -12,8 +12,65 @@ const Template: ComponentStory<typeof TextareaInput> = (args) => <TextareaInput 
 export const Default = Template.bind({});
 
 Default.args = {
-  label: 'Label',
+  label: 'Default',
   name: 'name',
-  maxByteSize: 100,
+  placeholder: '텍스트를 입력해주세요',
   error: false,
+};
+
+export const Disabled = Template.bind({});
+
+Disabled.args = {
+  label: 'Disabled',
+  name: 'disabled',
+  placeholder: '비활성화 된 텍스트 에어리어',
+  disabled: true,
+};
+
+export const Error = Template.bind({});
+
+Error.args = {
+  label: 'Error',
+  name: 'error',
+  value: '잘못된 텍스트',
+  error: true,
+};
+
+export const WithCounter = Template.bind({});
+
+WithCounter.args = {
+  label: 'With Counter',
+  name: 'withCounter',
+  placeholder: '텍스트를 입력해주세요',
+  value: 'Test text',
+  byteSize: 9,
+  maxByteSize: 100,
+  enableByteCount: true,
+};
+
+export const WithSaveButton = Template.bind({});
+
+WithSaveButton.args = {
+  label: 'With Save Button',
+  name: 'withSaveButton',
+  placeholder: '텍스트를 입력해주세요',
+  value: 'Test text',
+  byteSize: 9,
+  maxByteSize: 100,
+  enableByteCount: true,
+  enableSaveButton: true,
+};
+
+export const WithImageUploadButton = Template.bind({});
+
+WithImageUploadButton.args = {
+  label: 'With Image Upload Button',
+  name: 'withImageUploadButton',
+  placeholder: '텍스트를 입력해주세요',
+  value: 'Test text',
+  byteSize: 9,
+  maxByteSize: 100,
+  enableByteCount: true,
+  enableSaveButton: true,
+  enableImageUpload: true,
 };
