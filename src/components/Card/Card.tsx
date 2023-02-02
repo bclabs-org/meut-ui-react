c8c6a6f5d17f296e149a0095fb722906ed8487bd
miakim <mia.kim@bclabs.co.kr>
2023-02-02 21:30:08 +0900

    feat: gradient 색상 두가지 추가, card 테마 색상 추가

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 
 type CardProps = {
   children: React.ReactNode;
+  color: string;
 };
 
-const Card: React.FC<CardProps> = ({ children }) => (
-  <div className="flex flex-col col-span-3 gap-y-5 cursor-pointer p-5 rounded-xl border border-gray-300 bg-tertiary hover:border-2 hover:border-primary hover:m-[-1px] hover:bg-white hover:active:border hover:active:border-gray-300 hover:active:bg-tertiary hover:active:m-0">
+const Card: React.FC<CardProps> = ({ children, color }) => {
+  let cardColor = 'bg-tertiary';
+  let cardHoverColor = 'hover:bg-white';
+  let cardActiveColor = 'hover:active:bg-tertiary'
+  let borderHoverColor = 'hover:border-primary'
+  switch (color) {
+    case 'gradient-buy':
+      cardColor = 'bg-gradient-buy';
+      cardHoverColor = 'hover:bg-gradient-buy-hover';
+      cardActiveColor = 'hover:active:bg-gradient-buy'
+      break;
+    case 'gradient-sell':
+      cardColor = 'bg-gradient-sell';
+      cardHoverColor = 'hover:bg-gradient-sell-hover';
+      cardHoverColor = 'hover:active:bg-gradient-sell'
+      borderHoverColor ='hover:border-pink-600'
+      break;
+    default:
+      cardColor = 'bg-tertiary';
+      cardHoverColor = 'hover:bg-white';
+      cardActiveColor = 'hover:active:bg-tertiary'
+      break;
+  }
+
+  return (
+  <div className={`flex flex-col col-span-3 gap-y-5 cursor-pointer p-5 rounded-xl border border-gray-300 ${cardColor} hover:border-2 ${borderHoverColor} hover:m-[-1px] ${cardHoverColor} hover:active:border hover:active:border-gray-300 ${cardActiveColor} hover:active:m-0`}>
     {children}
   </div>
-);
+)};
 
 export default Card;
