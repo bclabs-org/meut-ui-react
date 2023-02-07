01506fb93de71df881b74b2951ec38f04da6b880
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-07 08:29:46 +0000

    chore: update bundle file

--- lib/Card/Card.d.ts
+++ lib/Card/Card.d.ts
@@ -2,6 +2,7 @@ import React from 'react';
 type CardProps = {
     children: React.ReactNode;
     color?: string;
+    className?: string;
 };
 declare const Card: React.FC<CardProps>;
 export default Card;
