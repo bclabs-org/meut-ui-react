ac45e80fe30052bc4d94f021c4c1743ee2a569c5
kimpaul <paul.kim@bclabs.co.kr>
2023-02-08 19:15:24 +0900

    PLAYV-1378 #comment "Chip 컴포넌트 텍스트만 있는 형태 추가"

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -3,31 +3,30 @@ import React from 'react';
 type ChipProps = {
   label: string;
   selected: boolean;
-  count: number;
+  count?: number;
   handleClick?: () => void;
-}
+};
 
-const Chip: React.FC<ChipProps> = ({
-  label,
-  selected,
-  count,
-  handleClick
-}) => (
-    <button
-      onClick={handleClick}
-      className={`group inline-flex items-center rounded-full px-4 py-2 cursor-pointer ${
+const Chip: React.FC<ChipProps> = ({ label, selected, count, handleClick }) => (
+  <button
+    onClick={handleClick}
+    className={`group inline-flex items-center rounded-full px-4 py-2 cursor-pointer ${
       selected
         ? 'bg-primary text-onPrimary font-semibold'
-        : 'bg-tertiary text-base font-medium text-onTertiary hover:bg-secondary focus:bg-primary focus:text-onPrimary focus:font-semibold'}`}>
-        {label}
-        <span className={`px-3 py-1 ml-1 rounded-[10px] text-onTertiary text-xs font-medium ${
-          selected
-            ? 'bg-white'
-            : 'bg-tertiary group-focus:bg-white'
-        }`}>
-          {count}
-        </span>
-    </button>
-  )
+        : 'bg-tertiary text-base font-medium text-onTertiary hover:bg-secondary focus:bg-primary focus:text-onPrimary focus:font-semibold'
+    }`}
+  >
+    {label}
+    {count ? (
+      <span
+        className={`px-3 py-1 ml-1 rounded-[10px] text-onTertiary text-xs font-medium ${
+          selected ? 'bg-white' : 'bg-tertiary group-focus:bg-white'
+        }`}
+      >
+        {count}
+      </span>
+    ) : null}
+  </button>
+);
 
 export default Chip;
