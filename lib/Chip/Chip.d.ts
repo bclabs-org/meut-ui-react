00318fc7d518a0ccdf9e7f00302d6eaf84c49571
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-09 06:55:40 +0000

    chore: update bundle file

--- lib/Chip/Chip.d.ts
+++ lib/Chip/Chip.d.ts
@@ -2,8 +2,9 @@ import React from 'react';
 type ChipProps = {
     label: string;
     selected: boolean;
-    count: number;
+    count?: number;
     handleClick?: () => void;
+    className?: string;
 };
 declare const Chip: React.FC<ChipProps>;
 export default Chip;
