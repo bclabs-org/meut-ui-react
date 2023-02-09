dbe66ae3bfa543ae56007559eba2eb5003d66373
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-09 07:05:04 +0000

    chore: update bundle file

--- lib/Tabs/Tabs.d.ts
+++ lib/Tabs/Tabs.d.ts
@@ -3,6 +3,7 @@ type TabsProps = {
     tabNames: string[];
     selectedTab: string;
     setSelectedTab: React.Dispatch<React.SetStateAction<string>>;
+    className?: string;
 };
 declare const Tabs: React.FC<TabsProps>;
 export default Tabs;
