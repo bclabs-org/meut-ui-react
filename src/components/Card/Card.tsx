2741fa51616fe51bcc0c8c8bb06fae1227aa9dbc
miakim <mia.kim@bclabs.co.kr>
2023-02-13 16:49:03 +0900

    feat: 카드 사이즈 추가

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 
 type CardProps = {
   children: React.ReactNode;
+  size?: 'medium' | 'large';
 };
 
-const Card: React.FC<CardProps> = ({ children }) => (
-  <div className="w-[343px] grid gap-y-5 cursor-pointer p-5 rounded-400 border border-gray-300 bg-tertiary hover:border-2 hover:border-primary hover:bg-white hover:active:border hover:active:border-gray-300 hover:active:bg-tertiary">
+const Card: React.FC<CardProps> = ({ children, size = 'medium' }) => {
+  let cardSize;
+  switch (size) {
+    case 'medium':
+      cardSize = 'w-[343px]';
+      break;
+    case 'large':
+      cardSize = 'w-[389.5px]';
+      break;
+    default:
+      cardSize = 'w-[343px]';
+  }
+  return (
+  <div className={`${cardSize} grid gap-y-5 cursor-pointer p-5 rounded-400 border border-gray-300 bg-tertiary hover:border-2 hover:border-primary hover:bg-white hover:active:border hover:active:border-gray-300 hover:active:bg-tertiary`}>
     {children}
   </div>
-);
+)};
 
 export default Card;
