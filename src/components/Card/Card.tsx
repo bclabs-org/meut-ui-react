66eae83792f2aee3ff19f1c691b6f5c84299d3b0
miakim <mia.kim@bclabs.co.kr>
2023-02-14 13:44:58 +0900

    feat: 카드 사이즈 입력받도록 수정

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
   size?: string;
 };
 
-const Card: React.FC<CardProps> = ({ children, color, className, size='medium' }) => {
+const Card: React.FC<CardProps> = ({ children, color, className, size='w-[343px]' }) => {
   let cardColor = 'bg-tertiary';
   let cardHoverColor = 'hover:bg-white';
   let cardActiveColor = 'hover:active:bg-tertiary';
@@ -31,22 +31,9 @@ const Card: React.FC<CardProps> = ({ children, color, className, size='medium' }
       break;
   }
 
-  let cardSize;
-  switch (size) {
-    case 'medium':
-      cardSize = 'w-[343px]';
-      break;
-    case 'large':
-      cardSize = 'w-[389.5px]';
-      break;
-    default:
-      cardSize = 'w-[343px]';
-  }
-
-
   return (
     <div
-      className={`flex flex-col col-span-3 ${cardSize} gap-y-5 cursor-pointer p-5 rounded-xl border border-gray-300 ${cardColor} hover:border-2 ${borderHoverColor} hover:m-[-1px] ${cardHoverColor} hover:active:border hover:active:border-gray-300 ${cardActiveColor} hover:active:m-0 ${className}`}
+      className={`flex flex-col col-span-3 ${size} gap-y-5 cursor-pointer p-5 rounded-xl border border-gray-300 ${cardColor} hover:border-2 ${borderHoverColor} hover:m-[-1px] ${cardHoverColor} hover:active:border hover:active:border-gray-300 ${cardActiveColor} hover:active:m-0 ${className}`}
     >
       {children}
     </div>
