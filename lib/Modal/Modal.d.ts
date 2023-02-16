fcc028e7cecac6ad090149c9dcc1c7549a0b2768
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-16 02:25:29 +0000

    chore: update bundle file

--- lib/Modal/Modal.d.ts
+++ lib/Modal/Modal.d.ts
@@ -3,6 +3,7 @@ type ModalProps = {
     children: React.ReactNode;
     isModalOpen: boolean;
     setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    onOutsideClick?: boolean;
 };
 declare const Modal: React.FC<ModalProps>;
 export default Modal;
