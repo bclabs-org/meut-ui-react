b7970a895296b6bb4e71d88817b7361fabd10239
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-20 05:33:12 +0000

    chore: update bundle file

--- lib/Dropdown/Dropdown.d.ts
+++ lib/Dropdown/Dropdown.d.ts
@@ -6,6 +6,7 @@ type DropdownProps = {
     placeholder: string;
     selected: string;
     setSelected: React.Dispatch<React.SetStateAction<string>>;
+    handleChange?: () => any;
 };
 declare const Dropdown: React.FC<DropdownProps>;
 export default Dropdown;
