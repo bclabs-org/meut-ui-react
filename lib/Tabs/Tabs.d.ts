27f0f0c545f5f129cea451ad0a94a09646bb5c0f
bclabs-mia <mia.kim@bclabs.co.kr>
2023-02-23 00:58:08 +0000

    chore: update bundle file

--- lib/Tabs/Tabs.d.ts
+++ lib/Tabs/Tabs.d.ts
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 type TabsProps = {
     tabNames: string[];
     selectedTab: string;
     setSelectedTab: React.Dispatch<React.SetStateAction<string>>;
     className?: string;
+    forwardedRef?: ForwardedRef<HTMLDivElement>;
 };
 declare const Tabs: React.FC<TabsProps>;
 export default Tabs;
