6e36124e0a2f2ecf4ea746ebc214f5f2b91c31a0
bclabs-theo <theo.kim@bclabs.co.kr>
2023-03-22 13:56:49 +0900

    CNVSTR-1868 #comment width:718px인 사이즈 추가를 위해 widthSize props 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,6 +5,7 @@ type ModalProps = {
   children: React.ReactNode;
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  widthSize?: string;
   onOutsideClick?: boolean;
 };
 
@@ -12,49 +13,64 @@ const Modal: React.FC<ModalProps> = ({
   children,
   isModalOpen,
   setIsModalOpen,
+  widthSize = "medium",
   onOutsideClick = true,
-}) => (
-  <Transition.Root show={isModalOpen} as={Fragment}>
+}) => {
+  let width:string;
+
+  switch(widthSize) {
+    case "medium" :
+      width = "w-[468px]"
+          break;
+    case "large":
+      width = "w-[718px]"
+          break;
+    default:
+      throw Error("invalid width size pros");
+  }
+
+  return (<Transition.Root show={isModalOpen} as={Fragment}>
     <Dialog
-      as="div"
-      className="relative z-10"
-      onClose={() => null}
-      onClick={(e) => {
-        e.stopPropagation();
-        setIsModalOpen(!onOutsideClick);
-      }}
+        as="div"
+        className="relative z-10"
+        onClose={() => null}
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsModalOpen(!onOutsideClick);
+        }}
     >
       <Transition.Child
-        as={Fragment}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
       >
-        <div className="fixed inset-0 bg-overlay transition-opacity" />
+        <div className="fixed inset-0 bg-overlay transition-opacity"/>
       </Transition.Child>
 
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full justify-center text-center items-center py-32">
           <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <Dialog.Panel className="w-[468px] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+            <Dialog.Panel
+                className={`${width} relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all`}>
               <div className="py-8 px-10">{children}</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
       </div>
     </Dialog>
-  </Transition.Root>
-);
+  </Transition.Root>)
+}
 
 export default Modal;
