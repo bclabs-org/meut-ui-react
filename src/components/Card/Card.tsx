64250779f9812f15d18b5ee19c49cde807e9b414
miakim <mia.kim@bclabs.co.kr>
2023-03-27 12:10:05 +0900

    test

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -25,10 +25,9 @@ const Card: React.FC<CardProps> = ({ children, color, className, size='w-[343px]
       borderHoverColor = 'hover:border-pink-600';
       break;
     default:
-      cardColor = 'bg-gradient-sell';
-      cardHoverColor = 'hover:bg-gradient-sell-hover';
-      cardHoverColor = 'hover:active:bg-gradient-sell';
-      borderHoverColor = 'hover:border-pink-600';
+      cardColor = 'bg-tertiary';
+      cardHoverColor = 'hover:bg-white';
+      cardActiveColor = 'hover:active:bg-tertiary';
       break;
   }
 
