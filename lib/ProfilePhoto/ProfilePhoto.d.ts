5dfeb7a9a2b78ff790bfa849ff1dc51a7527debd
bclabs-mia <mia.kim@bclabs.co.kr>
2023-04-07 09:41:14 +0000

    chore: update bundle file

--- lib/ProfilePhoto/ProfilePhoto.d.ts
+++ lib/ProfilePhoto/ProfilePhoto.d.ts
@@ -3,6 +3,7 @@ import './style.css';
 type ProfilePhotoProps = {
     size: 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | '2x-large';
     imageFile: string;
+    rounded?: boolean;
 };
 declare const ProfilePhoto: React.FC<ProfilePhotoProps>;
 export default ProfilePhoto;
