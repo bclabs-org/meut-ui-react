2fb9cf7084f26cddaf1631987a87a98548d5e3bf
miakim <mia.kim@bclabs.co.kr>
2023-04-11 09:55:57 +0900

    fix: bundle, *.d.ts 파일 삭제, git에 올리지 않도록 수정

--- lib/Badge/BadgeLabel.d.ts
+++
@@ -1,7 +0,0 @@
-interface BadgeLabelProps {
-    text: string;
-    status: 'attention' | 'disabled' | 'warning' | 'information';
-    className: string;
-}
-declare const BadgeLabel: ({ text, status, className }: BadgeLabelProps) => JSX.Element;
-export default BadgeLabel;
