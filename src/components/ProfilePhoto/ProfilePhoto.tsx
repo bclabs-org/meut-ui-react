d94c706b537267d0a0d3ea47f869eb5d8002c243
kimpaul <paul.kim@bclabs.co.kr>
2023-04-27 17:40:12 +0900

    CNVSTR-2117 #comment "뮤트 ProfilePhoto 컴포넌트에 min 사이즈 추가"

--- src/components/ProfilePhoto/ProfilePhoto.tsx
+++ src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -10,22 +10,22 @@ const ProfilePhoto: React.FC<ProfilePhotoProps> = ({ size, imageFile, rounded }:
   let profilePhotoSize;
   switch (size) {
     case 'x-small':
-      profilePhotoSize = 'w-6 h-6';
+      profilePhotoSize = 'w-6 h-6 min-w-[6px] min-h-[6px]';
       break;
     case 'small':
-      profilePhotoSize = 'w-8 h-8';
+      profilePhotoSize = 'w-8 h-8 min-w-[8px] min-h-[8px]';
       break;
     case 'medium':
-      profilePhotoSize = 'w-10 h-10';
+      profilePhotoSize = 'w-10 h-10 min-w-[10px] min-h-[10px]';
       break;
     case 'large':
-      profilePhotoSize = 'w-16 h-16';
+      profilePhotoSize = 'w-16 h-16 min-w-[16px] min-h-[16px]';
       break;
     case 'x-large':
-      profilePhotoSize = 'w-32 h-32';
+      profilePhotoSize = 'w-32 h-32 min-w-[32px] min-h-[32px]';
       break;
     case '2x-large':
-      profilePhotoSize = 'w-[148px] h-[148px]';
+      profilePhotoSize = 'w-[148px] h-[148px] min-w-[148px] min-h-[148px]';
       break;
     default:
       throw Error('invalid size value');
