97be6323331c182066e8b33b6791e99dd98637d7
kimpaul <paul.kim@bclabs.co.kr>
2023-10-20 11:32:26 +0900

    CNVSTR-2713 #comment "feat: context provider 추가 children으로 Tab 받도록 변경"

--- src/components/TabsV2/TabsV2.tsx
+++ src/components/TabsV2/TabsV2.tsx
@@ -1,50 +1,45 @@
 import React from 'react';
 import classNames from 'classnames';
+import { TabsV2Context } from './TabsV2Context';
 
 type TabsV2Props = {
-  tabData: Array<{ key: string; label: string }>;
-  selectedTab: string;
-  handleClick: (tab: string) => void;
+  activeTab: string;
+  onChange: (tab: string) => void;
   borderTop?: boolean;
   borderBottom?: boolean;
   bgColor?: boolean;
   shadow?: 'default' | 'side';
+  children?: React.ReactNode;
 };
 
 const TabsV2: React.FC<TabsV2Props> = ({
-  tabData,
-  selectedTab,
-  handleClick,
+  activeTab,
+  onChange,
   borderTop = false,
   borderBottom = false,
   bgColor = false,
   shadow,
-}) => (
-  <div
-    className={classNames('grid', `grid-cols-${tabData.length}`, 'h-16', 'bg-white', {
-      'border-t': borderTop,
-      'border-b': borderBottom,
-      'bg-gray-50': bgColor,
-      'shadow-[-2px_0_1px_-1px_rgba(0,0,0,0.1),2px_0_1px_-1px_rgba(0,0,0,0.1)]': shadow === 'side',
-      'shadow-[0_1px_1px_0_rgba(0,0,0,0.06),0_1px_2px_0_rgba(0,0,0,0.1)]': shadow === 'default',
-    })}
-  >
-    {tabData.map((tab) => (
-      <button
-        key={tab.key}
-        onClick={(): void => handleClick(tab.key)}
-        className={classNames(
-          'flex text-base justify-center hover:border-b-2 hover:border-emerald-400 font-medium hover:font-semibold active:font-semibold items-center cursor-pointer py-2',
-          selectedTab === tab.key
-            ? 'font-semibold text-black border-b-2 border-primary hover:border-primary'
-            : 'text-gray-700 border-b-2 border-transparent border-gray-200'
-        )}
-        aria-label={tab.label}
+  children,
+}) => {
+  const childrenCount = React.Children.count(children);
+
+  return (
+    <TabsV2Context.Provider value={{ activeTab, onChange }}>
+      <div
+        className={classNames('grid', `grid-cols-${childrenCount}`, 'h-16', {
+          'border-t': borderTop,
+          'border-b': borderBottom,
+          'bg-gray-50': bgColor,
+          'bg-white': !bgColor,
+          'shadow-[-2px_0_1px_-1px_rgba(0,0,0,0.1),2px_0_1px_-1px_rgba(0,0,0,0.1)]':
+            shadow === 'side',
+          'shadow-[0_1px_1px_0_rgba(0,0,0,0.06),0_1px_2px_0_rgba(0,0,0,0.1)]': shadow === 'default',
+        })}
       >
-        {tab.label}
-      </button>
-    ))}
-  </div>
-);
+        {children}
+      </div>
+    </TabsV2Context.Provider>
+  );
+};
 
 export default TabsV2;
