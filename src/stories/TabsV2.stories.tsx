95a5b614568e39c2f2d1f5ecff89d52d334a1476
kimpaul <paul.kim@bclabs.co.kr>
2023-10-20 11:34:26 +0900

    CNVSTR-2713 #comment "feat:스토리북에 번역 케이스 추가"

--- src/stories/TabsV2.stories.tsx
+++ src/stories/TabsV2.stories.tsx
@@ -2,50 +2,89 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import TabsV2 from '../components/TabsV2/TabsV2';
+import TabV2 from '../components/TabV2/TabV2';
+import Button from '../components/Button/Button';
+import Toggle from '../components/Toggle/Toggle';
+import Card from "../components/Card/Card";
 
 export default {
-    title: 'Components/TabsV2',
-    component: TabsV2,
+  title: 'Components/TabsV2',
+  component: TabsV2,
 } as ComponentMeta<typeof TabsV2>;
 
-const Template: ComponentStory<typeof TabsV2> = (args) => {
-    const [selectedTab, setSelectedTab] = React.useState(args.selectedTab);
-
-    const handleClick = (tabName: string):void => {
-        setSelectedTab(tabName);
-    }
-
-    return <TabsV2 {...args} selectedTab={selectedTab} handleClick={handleClick} />
+const translationData = {
+  ko: {
+    performance: '퍼포먼스',
+    risk: '리스크',
+    analytics: '분석',
+  },
+  en: {
+    performance: 'Performance',
+    risk: 'Risk',
+    analytics: 'Analytics',
+  },
 };
 
-const SampleTabData = [{key:'performance', label:"Performance", }, {key:'risk', label:"Risk", }, {key:'analytics', label:"Analytics",}]
+const Template: ComponentStory<typeof TabsV2> = (args) => {
+  const [selectedTab, setSelectedTab] = React.useState(args.activeTab);
+  const [currentLanguage, setCurrentLanguage] = React.useState<'en' | 'ko'>('en');
+
+  const mockT = (key: string): string => {
+    return translationData[currentLanguage][key];
+  };
+
+  const handleClick = (tabName: string): void => {
+    setSelectedTab(tabName);
+  };
+
+  return (
+    <>
+      <TabsV2 {...args} activeTab={selectedTab} onChange={handleClick}>
+        <TabV2 label={'performance'}>{mockT('performance')}</TabV2>
+        <TabV2 label={'risk'}>{mockT('risk')}</TabV2>
+        <TabV2 label={'analytics'}>{mockT('analytics')}</TabV2>
+      </TabsV2>
+      <Card className="mt-12 flex justify-between">
+        <div className="py-2 flex justify-between font-medium text-gray-700">
+          <p>activeTab</p>
+          <p className="text-emerald-500 font-semibold">{selectedTab}</p>
+        </div>
+        <div className="flex items-center gap-2 justify-between">
+          <span className="font-medium text-gray-700">{currentLanguage === 'ko' ? '번역 해제' : '한국어로 번역'}</span>
+          <Toggle
+            enabled={currentLanguage === 'ko'}
+            setEnabled={() => setCurrentLanguage(currentLanguage === 'ko' ? 'en' : 'ko')}
+            text={'한국어로 번역'}
+          />
+        </div>
+      </Card>
+    </>
+  );
+};
+
 export const Default = Template.bind({});
 Default.args = {
-    tabData: SampleTabData,
-    selectedTab: 'performance',
-    borderTop: true,
-    bgColor: true,
-}
+  activeTab: 'performance',
+  borderTop: true,
+  bgColor: true,
+};
 
 export const WithShadow = Template.bind({});
 WithShadow.args = {
-    tabData: SampleTabData,
-    selectedTab: 'performance',
-    shadow: true,
-}
+  activeTab: 'performance',
+  shadow: 'default',
+};
 
 export const WithBorder = Template.bind({});
 WithBorder.args = {
-    tabData: SampleTabData,
-    selectedTab: 'performance',
-    borderTop: true,
-    borderBottom: true,
-}
+  activeTab: 'performance',
+  borderTop: true,
+  borderBottom: true,
+};
 
-export  const WithBackgroundColor = Template.bind({});
+export const WithBackgroundColor = Template.bind({});
 WithBackgroundColor.args = {
-    tabData: SampleTabData,
-    selectedTab: 'performance',
-    bgColor: true,
-    shadow: true,
-}
+  activeTab: 'performance',
+  bgColor: true,
+  shadow: 'default',
+};
