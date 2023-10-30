a0f88403a43a46f866f153f6da3bd667b508a92d
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 10:10:21 +0900

    fix: lint fix -> JSX.Element to React.FC

--- lib/Stat/Stat.d.ts
+++ lib/Stat/Stat.d.ts
@@ -1,16 +1,31 @@
+import React from 'react';
+
 type StatProps = {
-    type?: 'default' | 'negative' | 'positive';
-    size: 'small' | 'medium' | 'large' | 'x-large' | '2x-large';
-    label?: string;
-    data: string;
-    className?: string;
-    labelClassName?: string;
-    isDataFirst?: boolean;
-    ticker?: string;
-    suffix?: string;
-    tooltipText?: string;
-    tooltipDisabled?: boolean;
-    prefix?: string;
+  type?: 'default' | 'negative' | 'positive';
+  size: 'small' | 'medium' | 'large' | 'x-large' | '2x-large';
+  label?: string;
+  data: string;
+  className?: string;
+  labelClassName?: string;
+  isDataFirst?: boolean;
+  ticker?: string;
+  suffix?: string;
+  tooltipText?: string;
+  tooltipDisabled?: boolean;
+  prefix?: string;
 };
-declare const Stat: ({ type, size, label, data, className, labelClassName, isDataFirst, ticker, suffix, prefix, tooltipText, tooltipDisabled, }: StatProps) => JSX.Element;
+declare const Stat: ({
+  type,
+  size,
+  label,
+  data,
+  className,
+  labelClassName,
+  isDataFirst,
+  ticker,
+  suffix,
+  prefix,
+  tooltipText,
+  tooltipDisabled,
+}: StatProps) => React.FC<StatProps>;
 export default Stat;
