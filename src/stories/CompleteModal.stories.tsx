9a2b2a1068cfb8a492d1808e49d44a8a073c5c9d
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 09:54:30 +0900

    add: complete modal story

---
+++ src/stories/CompleteModal.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { CompleteModal } from '../components/Modal';
+
+export default {
+  title: 'Components/CompleteModal',
+  component: CompleteModal,
+} as ComponentMeta<typeof CompleteModal>;
+
+const Template: ComponentStory<typeof CompleteModal> = (args) => <CompleteModal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Completed',
+  message:
+    'Your transaction has been completed.Your transaction has been completed.Your transaction has been completed.Your transaction has been completed.',
+  buttonText: 'Close',
+  isModalOpen: true,
+  setIsModalOpen: (): void => {},
+};
