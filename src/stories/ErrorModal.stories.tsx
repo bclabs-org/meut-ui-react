5e426d153740a50309a091c6913df4e85a8ca1d5
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 09:54:54 +0900

    add: error modal story

---
+++ src/stories/ErrorModal.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ErrorModal } from '../components/Modal';
+
+export default {
+  title: 'Components/ErrorModal',
+  component: ErrorModal,
+} as ComponentMeta<typeof ErrorModal>;
+
+const Template: ComponentStory<typeof ErrorModal> = (args) => <ErrorModal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Network error',
+  message: 'An unknown network error has occured. Please try again later.',
+  isModalOpen: true,
+  leftButtonText: 'My purchases',
+  rightButtonText: 'Continue browsing',
+};
