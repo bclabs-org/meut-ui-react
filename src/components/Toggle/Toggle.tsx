d31b73bf39b84cbdc40c63e5e0f8f710c71a39cf
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 13:57:58 +0900

    fix: tooltip 설정 간소화 (속도가 동작 속도 느리게 느껴짐)

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,46 +1,32 @@
+import React from 'react';
+
 import { Switch } from '@headlessui/react';
-import React, { useState } from 'react';
+import classNames from 'classnames';
 
 type ToggleProps = {
   enabled: boolean;
   setEnabled: React.Dispatch<React.SetStateAction<boolean>>;
-  text: string;
-  disabled?: boolean;
+  text?: string;
 };
 
-const Toggle: React.FC<ToggleProps> = ({
-  enabled,
-  setEnabled,
-  text,
-  disabled = false,
-}: ToggleProps) => {
-  const [isHover, setIsHover] = useState(false);
-
-  return (
-    <Switch
-      checked={enabled}
-      onChange={disabled ? undefined : setEnabled}
-      className={`${disabled ? 'opacity-40 cursor-default' : 'cursor-pointer'} ${
-        enabled ? 'bg-primary' : 'bg-gray-200'
-      } relative inline-flex h-6 w-11 flex-shrink-0 rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
-      onMouseOver={disabled ? undefined : () => setIsHover(true)}
-      onMouseLeave={disabled ? undefined : () => setIsHover(false)}
-    >
-      <span className="sr-only">{text}</span>
-      <span
-        aria-hidden="true"
-        className={`${
-          enabled ? 'translate-x-5' : 'translate-x-0'
-        } pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-100 ease-in-out z-10`}
-      />
-      {isHover && !enabled && (
-        <div className="bg-emerald-100 opacity-60 rounded-full w-7 h-7 absolute -left-1 -top-1 transition duration-300 ease-in-out" />
+const Toggle: React.FC<ToggleProps> = ({ enabled, setEnabled, text }: ToggleProps) => (
+  <Switch
+    checked={enabled}
+    onChange={setEnabled}
+    className={classNames(
+      enabled ? 'bg-primary' : 'bg-gray-200',
+      'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none'
+    )}
+  >
+    <span className="sr-only">{text}</span>
+    <span
+      aria-hidden="true"
+      className={classNames(
+        enabled ? 'translate-x-5' : 'translate-x-0',
+        'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
       )}
-      {isHover && enabled && (
-        <div className="bg-emerald-100 opacity-60 rounded-full w-7 h-7 absolute -right-1 -top-1 transition duration-300 ease-in-out" />
-      )}
-    </Switch>
-  );
-};
+    />
+  </Switch>
+);
 
 export default Toggle;
