aa5d70b9449c7309be93f02404016f6f80533404
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 09:41:08 +0900

    fix: modal index fixed

--- lib/Modal/Modal.d.ts
+++ lib/Modal/Modal.d.ts
@@ -1,10 +1,12 @@
 import React from 'react';
+
 type ModalProps = {
-    children: React.ReactNode;
-    isModalOpen: boolean;
-    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    widthSize?: "medium" | "medium-large" | "large";
-    onOutsideClick?: boolean;
+  children: React.ReactNode;
+  isModalOpen: boolean;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  widthSize?: 'x-small' | 'small' | 'medium' | 'large' | 'x-large';
+  onOutsideClick?: boolean;
+  isCoverHeader?: boolean;
 };
 declare const Modal: React.FC<ModalProps>;
 export default Modal;
