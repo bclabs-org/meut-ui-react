ce1c3e5c7bc60a3ebf8885f4bb344343d93aa7f1
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 09:55:25 +0900

    add: Stat type

---
+++ lib/Stat/Stat.d.ts
@@ -0,0 +1,16 @@
+type StatProps = {
+    type?: 'default' | 'negative' | 'positive';
+    size: 'small' | 'medium' | 'large' | 'x-large' | '2x-large';
+    label?: string;
+    data: string;
+    className?: string;
+    labelClassName?: string;
+    isDataFirst?: boolean;
+    ticker?: string;
+    suffix?: string;
+    tooltipText?: string;
+    tooltipDisabled?: boolean;
+    prefix?: string;
+};
+declare const Stat: ({ type, size, label, data, className, labelClassName, isDataFirst, ticker, suffix, prefix, tooltipText, tooltipDisabled, }: StatProps) => JSX.Element;
+export default Stat;
