3b5a7fb67e67543a0a35ca7769c60fc74c4af4df
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 14:04:29 +0900

    fix: text props optional (문구 노출 안되어 꼭 필요한 props가 아닌 것으로 보임)

--- lib/Toggle/Toggle.d.ts
+++ lib/Toggle/Toggle.d.ts
@@ -3,7 +3,7 @@ import React from 'react';
 type ToggleProps = {
     enabled: boolean;
     setEnabled: React.Dispatch<React.SetStateAction<boolean>>;
-    text: string;
+    text?: string;
     disabled?: boolean;
 };
 declare const Toggle: React.FC<ToggleProps>;
