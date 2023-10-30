e6b36b23a6be4ccd1b029031d25e5853acef6bcf
tedlee <ted.lee@bclabs.co.kr>
2023-10-30 14:05:30 +0900

    fix: modal size props 수정

--- lib/Modal/Modal.d.ts
+++ lib/Modal/Modal.d.ts
@@ -1,12 +1,12 @@
 import React from 'react';
 
 type ModalProps = {
-  children: React.ReactNode;
-  isModalOpen: boolean;
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  widthSize?: 'x-small' | 'small' | 'medium' | 'large' | 'x-large';
-  onOutsideClick?: boolean;
-  isCoverHeader?: boolean;
+    children: React.ReactNode;
+    isModalOpen: boolean;
+    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    widthSize?: 'small' | 'medium';
+    onOutsideClick?: boolean;
+    isCoverHeader?: boolean;
 };
 declare const Modal: React.FC<ModalProps>;
 export default Modal;
