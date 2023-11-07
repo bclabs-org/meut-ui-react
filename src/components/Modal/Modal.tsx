31acc0a77522953117941d2498fd9e1686f15114
martin <martin.lim@bclabs.co.kr>
2023-11-07 10:27:43 +0900

    fix modal position on mobile

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -59,7 +59,7 @@ const Modal: React.FC<ModalProps> = ({
           />
         </Transition.Child>
         <div className={`fixed inset-0 ${isCoverHeader ? 'z-30' : 'z-10'} overflow-y-auto`}>
-          <div className="flex min-h-full justify-center text-center items-center py-32">
+          <div className="flex min-h-screen justify-center text-center items-center py-32">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -70,7 +70,7 @@ const Modal: React.FC<ModalProps> = ({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel
-                className={`${width} relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all`}
+                className={`${width} relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all h-fit`}
               >
                 <div className="py-8 px-10">{children}</div>
               </Dialog.Panel>
