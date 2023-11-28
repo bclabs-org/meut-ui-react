2c28d2329e0aa5867bfdb83dfe0eec2f496a80eb
kimpaul <paul.kim@bclabs.co.kr>
2023-11-28 12:20:40 +0900

    CNVSTR-2765 #comment feat:Toast 프로바이더 컴포넌트 생성

---
+++ src/components/Toast/ToastProvider.tsx
@@ -0,0 +1,24 @@
+import React, { useReducer } from 'react';
+
+import Toast from './Toast';
+import ToastDispatchContext from './ToastDispatchContext';
+
+import ToastReducer from './ToastReducer';
+import initialState from './values';
+
+interface ToastProviderProps {
+  children?: React.ReactNode;
+}
+
+const ToastProvider = ({ children }: ToastProviderProps) => {
+  const [state, dispatch] = useReducer(ToastReducer, initialState);
+
+  return (
+    <ToastDispatchContext.Provider value={{ dispatch }}>
+      {state.isAlertOpen && <Toast toastState={state} toastDispatch={dispatch} />}
+      {children}
+    </ToastDispatchContext.Provider>
+  );
+};
+
+export default ToastProvider;
