d2675194d3fc9ab01ca5d94c997dab2a8906de04
kimpaul <paul.kim@bclabs.co.kr>
2023-11-29 16:29:26 +0900

    CNVSTR-2765 #comment refactor: 토스트 스토리북 수정

--- src/stories/Toast.stories.tsx
+++ src/stories/Toast.stories.tsx
@@ -2,44 +2,66 @@ import React from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import ToastProvider from '../components/Toast/ToastProvider';
-import useToast from '../components/Toast/useToast';
 
 import Button from '../components/Button';
-import { ToastState } from '../components/Toast/types';
+import { toast } from '../components/Toast/ToastStore';
+
+type DefaultArgs = {
+  message: string;
+  subMessage: string;
+  type: "error" | "attention" | "completion" | "information"
+};
 
 export default {
   title: 'Components/Toast',
   component: ToastProvider,
-} as ComponentMeta<typeof ToastProvider>;
+} as ComponentMeta<typeof React.Component<DefaultArgs>>;
 
-const Template = ({ toastState }: { toastState: ToastState }) => {
-  const toast = useToast();
-
-  const handleToast = () => {
-    toast(toastState.message, toastState.color, toastState.subMessage);
-  };
+const Template: ComponentStory<typeof React.Component<DefaultArgs>> = (args) => {
+  const abc = 123;
 
   return (
     <div>
-      <Button onClick={handleToast} size={'large'}>
-        Popup Toast
-      </Button>
+      <ToastProvider />
+      <div className="flex flex-col gap-4">
+        <div>
+          <h2>Toast information</h2>
+          <code className="rounded bg-gray-100 text-xs p-1">
+            toast.info(&#39;info msg&#39;, &#39;subMessage?&#39;)
+          </code>
+         <Button size={'large'} onClick={() => toast.info('info msg')}>
+          Popup info
+         </Button>
+        </div>
+        <div>
+         <Button size={'large'} onClick={() => toast.completion('completion msg')}>
+          Popup completion
+         </Button>
+        </div>
+        <div>
+         <Button size={'large'} onClick={() => toast.attention('attention msg')}>
+          Popup attention
+         </Button>
+        </div>
+        <div>
+         <Button size={'large'} onClick={() => toast.error('error msg')}>
+          Popup error
+         </Button>
+        </div>
+        <div>
+        <Button size={'large'} onClick={() => toast(args.message, args.type, args.subMessage)}>
+          Popup Custom Toast!
+        </Button>
+        </div>
+      </div>
     </div>
   );
 };
 
-const ProviderTemplate = (args) => (
-  <ToastProvider>
-    <Template toastState={args.toastState} />
-  </ToastProvider>
-);
 
-export const Default: ComponentStory<any> = ProviderTemplate.bind({});
+export const Default: ComponentStory<typeof React.Component<DefaultArgs>> = Template.bind({});
 Default.args = {
-  toastState: {
-    message: 'This is a toast message',
-    subMessage: 'This is a sub message',
-    color: 'error',
-    isAlertOpen: true,
-  },
+  message: 'This is a toast message',
+  subMessage: 'This is a sub message',
+  type: 'error',
 };
