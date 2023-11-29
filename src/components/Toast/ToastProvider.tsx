370188964c6272b682b02971f1372606a2c3b3cd
kimpaul <paul.kim@bclabs.co.kr>
2023-11-29 17:44:51 +0900

    CNVSTR-2765 #comment feat: 토스트 높이 useState로 관리하도록 변경 / 높이 업데이트하는 함수, 총 높이 계산하는 함수 작성

--- src/components/Toast/ToastProvider.tsx
+++ src/components/Toast/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useSyncExternalStore } from 'react';
+import React, { useCallback, useMemo, useSyncExternalStore } from 'react';
 
 import { Portal } from '@headlessui/react';
 import Toast from './Toast';
@@ -6,18 +6,41 @@ import Toast from './Toast';
 import { toastStore } from './ToastStore';
 
 const ToastProvider = () => {
+  const [toastHeights, setToastHeights] = React.useState({});
   const toasts = useSyncExternalStore(
     toastStore.subscribe,
     toastStore.getSnapshot,
     toastStore.getServerSnapshot
   );
 
+  const handleHeightReady = (index: number, height: number) => {
+    setToastHeights((prevHeights) => ({ ...prevHeights, [index]: height }));
+  };
+
+  const getTopPos = (index: number) => {
+    const cumulativeHeight = Object.keys(toastHeights)
+      .slice(0, index)
+      .reduce((acc, curr) => acc + toastHeights[curr], 0);
+    const topOffSet = 24 * 4;
+    const gaps = index * 12;
+
+    return cumulativeHeight + gaps + topOffSet;
+  };
+
   return (
-    <Portal>
-      {toasts.map((toast, index) => (
-        <Toast key={toast.message} toastState={toast} index={index} />
-      ))}
-    </Portal>
+    toasts.length > 0 && (
+      <Portal as={'div'}>
+        {toasts.map((toast, index) => (
+          <Toast
+            key={toast.message}
+            toastState={toast}
+            index={index}
+            onHeightReady={handleHeightReady}
+            top={getTopPos(index)}
+          />
+        ))}
+      </Portal>
+    )
   );
 };
 
