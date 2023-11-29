0d35f6da0b39f18cce4b9e2e6b42799cc9e2dd1e
kimpaul <paul.kim@bclabs.co.kr>
2023-11-29 16:28:42 +0900

    CNVSTR-2765 #comment refactor: 토스트 컴포넌트가 익스터널 스토어에서 값을 받도록 변경

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -2,14 +2,15 @@ import React, { useEffect } from 'react';
 import { Portal } from '@headlessui/react';
 import classNames from 'classnames';
 import Alert from '../Alert';
-import { ToastAction, ToastState } from './types';
+import { ToastState } from './types';
+import { toastStore } from './ToastStore';
 
 type ToastProps = {
   toastState: ToastState;
-  toastDispatch: React.Dispatch<ToastAction>;
+  index: number;
 };
 
-const Toast = ({ toastState, toastDispatch }: ToastProps) => {
+const Toast = ({ toastState, index }: ToastProps) => {
   const [isMounted, setIsMounted] = React.useState(false);
   const [isCloseAnimationPlay, setIsCloseAnimationPlay] = React.useState(false);
   const timerRef = React.useRef<number | ReturnType<typeof setTimeout>>();
@@ -26,41 +27,65 @@ const Toast = ({ toastState, toastDispatch }: ToastProps) => {
     if (isMounted) {
       timerRef.current = setTimeout(() => {
         setIsCloseAnimationPlay(true);
-      }, 2000);
+      }, 20000);
     }
   }, [isMounted]);
 
   const cb1 = 'ease-[cubic-bezier(0.18,0.89,0.32,1.28)]';
   const cb2 = 'ease-[cubic-bezier(0.44, -0.15, 0.57, 1.43)]';
   const cb3 = 'ease-[cubic-bezier(0.34,1.56,0.64,1)]';
+  const cb4 = 'ease-[cubic-bezier(.58,-0.01,.35,1.55)]';
+
+  const toastRef = React.useRef<HTMLDivElement>(null);
+
+  const getTopPos = (_index: number) => {
+    const elementHeight = toastRef.current?.getBoundingClientRect().height || 0;
+    const previousElementHeight =
+      toastRef.current?.previousElementSibling?.clientHeight || elementHeight;
+
+    if (!isMounted) {
+      return 14 * 4 + previousElementHeight * _index;
+    }
+
+    if (isMounted && !isCloseAnimationPlay) {
+      const gapBetweenToast = index * 12;
+      return 24 * 4 + previousElementHeight * _index + gapBetweenToast;
+    }
+
+    if (isMounted && isCloseAnimationPlay) {
+      return 14 * 4 + previousElementHeight * _index;
+    }
+
+    return 0;
+  };
 
   return (
-    <Portal>
-      <div
-        className={classNames(
-          'transition-all duration-300 fixed z-50 left-1/2 -translate-x-1/2',
-          cb2,
-          !isMounted ? 'opacity-0 top-14' : '',
-          isMounted && !isCloseAnimationPlay ? 'opacity-100 top-24' : '',
-          isMounted && isCloseAnimationPlay ? 'opacity-0 top-16' : ''
-        )}
-        onTransitionEnd={() => {
-          if (isCloseAnimationPlay) {
-            setIsCloseAnimationPlay(false);
-            toastDispatch({ type: 'SET_TOAST_CLOSE' });
-          }
-        }}
-      >
-        <Alert
-          className="mx-auto"
-          text={toastState.message || ''}
-          subText={toastState.subMessage}
-          color={toastState.color as 'error'}
-          setIsAlertOpen={() => setIsCloseAnimationPlay(true)}
-          closeBtn
-        />
-      </div>
-    </Portal>
+    <div
+      className={classNames(
+        'fixed transition-all duration-300 z-50 left-1/2 -translate-x-1/2',
+        cb1,
+        !isMounted ? `opacity-0` : '',
+        isMounted && !isCloseAnimationPlay ? `opacity-100` : '',
+        isMounted && isCloseAnimationPlay ? `opacity-0` : ''
+      )}
+      style={{ top: `${getTopPos(index)}px` }}
+      onTransitionEnd={() => {
+        if (isCloseAnimationPlay) {
+          setIsCloseAnimationPlay(false);
+          toastStore.removeToast(toastState.message || '');
+        }
+      }}
+      ref={toastRef}
+    >
+      <Alert
+        className="mx-auto"
+        text={toastState.message || ''}
+        subText={toastState.subMessage}
+        color={toastState.type as 'error'}
+        setIsAlertOpen={() => setIsCloseAnimationPlay(true)}
+        closeBtn
+      />
+    </div>
   );
 };
 export default Toast;
