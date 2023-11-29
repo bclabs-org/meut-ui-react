e9d01b984d7fddde568ab9073aa1154e654aff4a
kimpaul <paul.kim@bclabs.co.kr>
2023-11-29 18:39:35 +0900

    CNVSTR-2765 #comment refactor: 함수에 useCallback 적용, 컴포넌트 리턴에 null 명시, 컴포넌트 리턴타입 명시

--- src/components/Toast/ToastProvider.tsx
+++ src/components/Toast/ToastProvider.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useMemo, useSyncExternalStore } from 'react';
+import React, { useCallback, useSyncExternalStore } from 'react';
 
 import { Portal } from '@headlessui/react';
 import Toast from './Toast';
 
 import { toastStore } from './ToastStore';
 
-const ToastProvider = () => {
+const ToastProvider: React.FC = () => {
   const [toastHeights, setToastHeights] = React.useState({});
   const toasts = useSyncExternalStore(
     toastStore.subscribe,
@@ -13,35 +13,36 @@ const ToastProvider = () => {
     toastStore.getServerSnapshot
   );
 
-  const handleHeightReady = (index: number, height: number) => {
+  const handleHeightReady = useCallback((index: number, height: number) => {
     setToastHeights((prevHeights) => ({ ...prevHeights, [index]: height }));
-  };
+  }, []);
 
-  const getTopPos = (index: number) => {
-    const cumulativeHeight = Object.keys(toastHeights)
-      .slice(0, index)
-      .reduce((acc, curr) => acc + toastHeights[curr], 0);
-    const topOffSet = 24 * 4;
-    const gaps = index * 12;
+  const getTopPos = useCallback(
+    (index: number) => {
+      const cumulativeHeight = Object.keys(toastHeights)
+        .slice(0, index)
+        .reduce((acc, curr) => acc + toastHeights[curr], 0);
+      const topOffSet = 24 * 4; // 화면 위쪽부터 아래로 96px
+      const gaps = index * 12;
 
-    return cumulativeHeight + gaps + topOffSet;
-  };
-
-  return (
-    toasts.length > 0 && (
-      <Portal as={'div'}>
-        {toasts.map((toast, index) => (
-          <Toast
-            key={toast.message}
-            toastState={toast}
-            index={index}
-            onHeightReady={handleHeightReady}
-            top={getTopPos(index)}
-          />
-        ))}
-      </Portal>
-    )
+      return cumulativeHeight + gaps + topOffSet;
+    },
+    [toastHeights]
   );
+
+  return toasts.length > 0 ? (
+    <Portal as={'div'}>
+      {toasts.map((toast, index) => (
+        <Toast
+          key={toast.message}
+          toastState={toast}
+          index={index}
+          onHeightReady={handleHeightReady}
+          top={getTopPos(index)}
+        />
+      ))}
+    </Portal>
+  ) : null;
 };
 
 export default ToastProvider;
