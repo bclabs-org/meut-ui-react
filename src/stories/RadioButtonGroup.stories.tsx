db536e2812c6937ed370de73e562e8efb2639e28
kimpaul <paul.kim@bclabs.co.kr>
2023-12-19 18:07:23 +0900

    CNVSTR-2786 #comment fix: 스토리북에 동시에 2개 이상의 RadioButtonGroup 컴포넌트를 사용할 경우 추가

--- src/stories/RadioButtonGroup.stories.tsx
+++ src/stories/RadioButtonGroup.stories.tsx
@@ -10,18 +10,37 @@ export default {
 
 const Template: ComponentStory<typeof RadioButtonGroup> = (args) => {
   const [selected, setSelected] = React.useState('1');
+  const [selected2, setSelected2] = React.useState('1');
   const handleChange = (value: string): void => {
     setSelected(value);
   };
 
+  const handleChange2 = (value: string): void => {
+    setSelected2(value);
+  };
+
   return (
     <>
-      <p className="font-medium text-gray-500">Current Select Id: {selected}</p>
+      <p>RadioButtonGroup</p>
+      <p className="mt-1 text-xs font-medium text-gray-500">Current Select Id: {selected}</p>
       <RadioButtonGroup
         {...args}
         onChange={handleChange}
         defaultCheckedId={selected}
         legend="numbers"
+        className="mt-4"
+      >
+        <RadioButton id={'1'} label={'one'} />
+        <RadioButton id={'2'} label={'two'} />
+        <RadioButton id={'3'} label={'three'} />
+      </RadioButtonGroup>
+      <p className="mt-8">RadioButtonGroup 2</p>
+      <p className="mt-1 text-xs font-medium text-gray-500">Current Select Id: {selected2}</p>
+      <RadioButtonGroup
+        {...args}
+        onChange={handleChange2}
+        defaultCheckedId={selected2}
+        legend="numberstwo"
         className="mt-4"
       >
         <RadioButton id={'1'} label={'one'} />
