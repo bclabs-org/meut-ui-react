ebb499919da5e81b09b3c5517e2f80c0f011342d
tedlee <ted.lee@bclabs.co.kr>
2024-02-20 16:18:36 +0900

    CNVSTR-2870 #comment 모달 닫기 버튼 옵션 X 아이콘 추가

--- src/components/Modal/ErrorModal.tsx
+++ src/components/Modal/ErrorModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
+import { XMarkIcon } from '@heroicons/react/20/solid';
 import Button from '../Button';
 
 type ModalProps = {
@@ -12,6 +13,7 @@ type ModalProps = {
   leftButtonOnClick: () => void;
   rightButtonOnClick?: () => void;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isXButton?: boolean;
   children?: React.ReactNode;
 };
 
@@ -24,8 +26,19 @@ const ErrorModal: React.FC<ModalProps> = ({
   setIsModalOpen,
   leftButtonOnClick,
   children,
+  isXButton,
 }) => (
-  <div className="flex flex-col gap-6">
+  <div className="relative flex flex-col gap-6">
+    {isXButton && (
+      <button
+        className={`absolute top-0 right-0`}
+        type="button"
+        onClick={() => setIsModalOpen(false)}
+        aria-label="Close Modal"
+      >
+        <XMarkIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+      </button>
+    )}
     <div className="flex flex-col gap-5 items-center ">
       <div>
         <div className={`p-3 bg-pink-50 rounded-full`}>
